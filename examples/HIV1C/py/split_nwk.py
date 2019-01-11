1a5c3ba1f264524d630c2c92ab29563800f5719a
Anna Zhukova <zhutchok@gmail.com>
2019-01-11 17:33:03 +0100

    tree pipeline with singularity for tars

--- examples/HIV1C/py/split_nwk.py
+++ examples/HIV1C/py/split_nwk.py
@@ -1,4 +1,3 @@
-import logging
 import os
 from Bio.Phylo import write, NewickIO
 
@@ -9,15 +8,11 @@ if '__main__' == __name__:
 
     parser.add_argument('--trees', required=True, type=str)
     parser.add_argument('--pattern', required=True, type=str)
-    parser.add_argument('--log', required=True, type=str)
     params = parser.parse_args()
-
-    logging.basicConfig(level=logging.INFO, format='%(asctime)s: %(message)s', datefmt="%Y-%m-%d %H:%M:%S")
 
     i = 0
     for tree in NewickIO.parse(params.trees):
         os.makedirs(os.path.dirname(params.pattern % i), exist_ok=True)
         write([tree], params.pattern % i, 'newick', plain=True)
         i += 1
-    with open(params.log, 'w+') as f:
-        f.write('Split a multi-tree newick file into %d one-tree files' % i)
+
