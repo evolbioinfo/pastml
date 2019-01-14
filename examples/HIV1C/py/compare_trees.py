964d14cc409ffffb27e8385c8ccaf73bb7660aaf
Anna Zhukova <zhutchok@gmail.com>
2019-01-14 16:31:35 +0100

    fixed tree comparison

--- examples/HIV1C/py/compare_trees.py
+++ examples/HIV1C/py/compare_trees.py
@@ -1,46 +1,52 @@
+import logging
 import os
+from multiprocessing.pool import ThreadPool
 
 import numpy as np
 import pandas as pd
-from scipy.misc import comb
+from Bio.Phylo import parse
 from ete3 import Tree
-
+from scipy.misc import comb
 
 if '__main__' == __name__:
     import argparse
 
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('--trees', required=True, type=str, nargs='+')
+    parser.add_argument('--trees', required=True, type=str)
+    parser.add_argument('--labels', required=True, type=str)
     parser.add_argument('--output', required=True, type=str)
     parser.add_argument('--qt', required=True, type=str)
     params = parser.parse_args()
 
-    results = pd.DataFrame(columns=['tree1', 'tree2', 'norm_rf', 'norm_qt'])
-    trees = list(params.trees)
-    prefix = os.path.commonprefix(trees)
-    path2tree = {_: Tree(_) for _ in trees}
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s: %(message)s', datefmt="%Y-%m-%d %H:%M:%S")
 
-    qt_total = comb(len(next(iter(path2tree.values()))), 4)
+    data, indices = [], []
+    trees = [Tree(_.format('newick')) for _ in parse(params.trees, 'newick')]
+    with open(params.labels, 'r') as f:
+        labels = f.readline().strip('\n').strip(' ').split('\t')
+    n = len(trees)
+    tri = np.zeros((n, n))
+    tri[np.tril_indices(n, 0)] = np.fromfile(params.qt, dtype=float, sep='\t')
+    tri /= comb(len(trees[0]), 4)
 
-    dm = np.fromfile(params.qt, dtype=float, sep='\t')
-    tri = np.zeros((len(trees), len(trees)))
-    tri[np.tril_indices(len(trees), 0)] = dm
+    def compare(args):
+        i, j = args
+        nqt = tri[j, i]
+        logging.info('Comparing {} to {}: quartet dist is {}'.format(i, j, nqt))
+        nrf = trees[i].compare(trees[j], unrooted=True)['norm_rf']
+        logging.info('Comparing {} to {}: RF dist is {}'.format(i, j, nrf))
+        return '{} vs {}'.format(labels[i], labels[j]), nrf, nqt
 
-    for i in range(len(trees) - 1):
-        path1 = trees[i]
-        tree1 = path2tree[path1]
-        path1 = path1[len(prefix):]
-        for j in range(i + 1, len(trees)):
-            path2 = trees[j]
-            tree2 = path2tree[path2]
-            path2 = path2[len(prefix):]
-            nrf = tree1.compare(tree2, unrooted=True)['norm_rf']
-            nqt = tri[j, i] / comb(len(tree1), 4)
-            results = results.append({'tree1': path1, 'tree2': path2, 'norm_rf': nrf, 'norm_qt': nqt}, ignore_index=True)
-    mrf = results['norm_rf'].mean()
-    mqt = results['norm_qt'].mean()
-    results = results.append({'tree1': 'mean', 'tree2': 'mean', 'norm_rf': mrf, 'norm_qt': mqt}, ignore_index=True)
-
-    results.to_csv(params.output, sep='\t', index=False)
+    with ThreadPool() as pool:
+        dists = pool.map(func=compare, iterable=((i, j) for i in range(n - 1)
+                                                 for j in range(i + 1, n)))
+    for label, nrf, nqt in dists:
+        data.append([nrf, nqt])
+        indices.append(label)
+    df = pd.DataFrame(data=data, columns=['norm_rf', 'norm_qt'], index=indices)
+    mrf = df['norm_rf'].mean()
+    mqt = df['norm_qt'].mean()
+    df.loc['mean', ['norm_rf', 'norm_qt']] = mrf, mqt
+    df.to_csv(params.output, sep='\t')
 
