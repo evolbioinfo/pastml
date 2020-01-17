a5ecf17e24833c81f488f3f3400618b9ffc7dce4
Anna Zhukova <zhutchok@gmail.com>
2020-01-17 10:40:28 +0100

    v1.9.22

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
         'Topic :: Software Development',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ],
-    version='1.9.21',
+    version='1.9.22',
     description='Ancestral character reconstruction and visualisation for rooted phylogenetic trees.',
     author='Anna Zhukova',
     author_email='anna.zhukova@pasteur.fr',
