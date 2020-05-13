a4e2d50bd00f528f7c86688ab39bc9f1e46d3b6c
Anna Zhukova <zhutchok@gmail.com>
2020-05-13 10:53:24 +0200

    adding more stats on the input tree

--- pastml/annotation.py
+++ pastml/annotation.py
@@ -1,13 +1,18 @@
 import logging
 
 import pandas as pd
+import numpy as np
 
 
 def get_forest_stats(forest):
-    len_sum = 0
+    len_sum_ext = 0
+    len_sum_int = 0
     num_zero_nodes = 0
     max_polynomy = 0
-    max_len = 0
+    max_len_ext = 0
+    max_len_int = 0
+    min_len_ext = np.inf
+    min_len_int = np.inf
     num_tips = 0
     num_nodes = 0
     num_zero_tips = 0
@@ -16,33 +21,53 @@ def get_forest_stats(forest):
     for tree in forest:
         for node in tree.traverse():
             num_nodes += 1
-            len_sum += node.dist
             max_polynomy = max(len(node.children), max_polynomy)
-            max_len = max(max_len, node.dist)
+
             if not node.dist:
                 num_zero_nodes += 1
 
             if node.is_leaf():
                 num_tips += 1
                 tip_len_sum += node.dist
-                if not node.dist:
+                if node.dist:
+                    min_len_ext = min(node.dist, min_len_ext)
+                    len_sum_ext += node.dist
+                    max_len_ext = max(max_len_ext, node.dist)
+                else:
                     num_zero_tips += 1
+            else:
+                if node.dist:
+                    min_len_int = min(node.dist, min_len_int)
+                    len_sum_int += node.dist
+                    max_len_int = max(max_len_int, node.dist)
 
-    avg_br_len = len_sum / (num_nodes - num_zero_nodes)
+    avg_len = (len_sum_ext + len_sum_int) / (num_nodes - num_zero_nodes)
+    avg_len_ext = len_sum_ext / (num_tips - num_zero_tips)
+    avg_len_int = len_sum_int / (num_nodes - num_tips - num_zero_nodes + num_zero_tips)
     logging.getLogger('pastml').debug('\n=============TREE STATISTICS===================\n'
                                       '\tnumber of tips:\t{}\n'
                                       '\tnumber of zero-branch tips:\t{}\n'
                                       '\tnumber of internal nodes:\t{}\n'
                                       '\tmax number of children per node:\t{}\n'
-                                      '\tmax branch length:\t{:.5f}\n'
+                                      '\tmax tip branch length:\t{:.5f}\n'
+                                      '\tmax internal branch length:\t{:.5f}\n'
+                                      '\tmin non-zero tip branch length:\t{:.5f}\n'
+                                      '\tmin non-zero internal branch length:\t{:.5f}\n'
+                                      '\tavg non-zero tip branch length:\t{:.5f}\n'
+                                      '\tavg non-zero internal branch length:\t{:.5f}\n'
                                       '\tavg non-zero branch length:\t{:.5f}.'
                                       .format(num_tips,
                                               num_zero_tips,
                                               num_nodes - num_tips,
                                               max_polynomy,
-                                              max_len,
-                                              avg_br_len))
-    return avg_br_len, num_nodes, num_tips
+                                              max_len_ext,
+                                              max_len_int,
+                                              min_len_ext,
+                                              min_len_int,
+                                              avg_len_ext,
+                                              avg_len_int,
+                                              avg_len))
+    return avg_len, num_nodes, num_tips
 
 
 def preannotate_forest(df, forest):
