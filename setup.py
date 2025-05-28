ab77500254340cd46fab9c2ea2dd1a6310d742d0
Anna Zhukova <zhutchok@gmail.com>
2025-05-28 12:57:44 +0200

    Constrained numpy version for compatibility with scipy

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ setup(
     url='https://github.com/evolbioinfo/pastml',
     keywords=['PASTML', 'visualisation', 'phylogeny', 'ancestral character reconstruction'],
     python_requires='>=3.10',
-    install_requires=['ete3>=3.1.1', 'pandas>=1.0.0', 'numpy==1.22', 'jinja2>=2.11.0', 'scipy==1.14.0', 'itolapi>=4.0.0', 'biopython>=1.70'],
+    install_requires=['ete3>=3.1.1', 'pandas>=1.0.0', 'numpy<2.3,>=1.23.5', 'jinja2>=2.11.0', 'scipy==1.14.0', 'itolapi>=4.0.0', 'biopython>=1.70'],
     entry_points={
             'console_scripts': [
                 'pastml = pastml.acr:main',
